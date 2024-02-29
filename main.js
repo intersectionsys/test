01f05802057b77f9c0b259d7a6f5158b0ca34dda
Rafael Andrade <rafael.mirapalheta@delage.com.br>
2024-02-29 17:01:07 -0300

    Card

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const $ = (query) => document.querySelector(query);
 const detailsModal = $(".details-modal");
+const cardModal = $(".card-modal");
 
 $("#details").addEventListener("click", () => {
   detailsModal.classList.add("open");
@@ -7,4 +8,12 @@ $("#details").addEventListener("click", () => {
 
 $("#closeDetails").addEventListener("click", () => {
   detailsModal.classList.remove("open");
+});
+
+$("#openCard").addEventListener("click", () => {
+  cardModal.classList.add("open");
+});
+
+$(".out").addEventListener("click", () => {
+  cardModal.classList.remove("open");
 });
